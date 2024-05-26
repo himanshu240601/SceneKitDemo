38c611d517d66e3a7af893c4f06c5db72c662b0f
Himanshu Goyal <goyahim69@gmail.com>
2024-05-26 10:37:34 +0530

    - first commit

--- SceneKitDemo/ViewController.swift
+++ SceneKitDemo/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    private var sceneView: SCNView!
 
+    // MARK: Lifecycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        // initialize scnview
+        sceneView = SCNView(frame: self.view.frame)
+        
+        // configure scene view
+        sceneView.allowsCameraControl = true
+        sceneView.autoenablesDefaultLighting = true
+        
+        // create a scnscene
+        let scene = SCNScene(named: "Lowpoly_tree_sample.obj")
+        sceneView.scene = scene
+        
+        sceneView.scene?.rootNode.scale = SCNVector3(x: 0.5, y: 0.5, z: 0.5)
+        
+        // add scene view to main parent view
+        self.view.addSubview(sceneView)
+    }
 }
-
